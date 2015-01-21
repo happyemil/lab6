d4086fd3cb4ae8ffea06cf7f193ceef2426a1387
Per Johansson <ratache@gmail.com>
2015-01-21 15:23:08 +0100

    Pers källkod

---
+++ libpower/include/libpower.h
@@ -0,0 +1,10 @@
+/*
+	libpower.h
+	Function declarations for libpower.c. 
+	Written by Per Johansson.
+*/
+#ifndef LIBPOWER_H
+#define LIBPOWER_H
+float calc_power_r(float volt, float resistance);
+float calc_power_i(float volt, float current);
+#endif
---
+++ libpower/src/libpower.c
@@ -0,0 +1,29 @@
+#include <stdlib.h>
+#include "../include/libpower.h"
+
+/*
+	Övning 6. Bibliotek 2
+	skrivet av Per Johansson
+*/
+
+/*
+	P=U^2/R
+*/
+float calc_power_r(float volt, float resistance){
+	float P = 0.0;
+	float R = resistance;
+	float U = volt;
+	P = (U * 2)/R;
+	return P;
+}
+
+/*
+	P=U*I
+*/
+float calc_power_i(float volt, float current){
+	float P = 0.0;
+	float U = volt;
+	float I = current;
+	P = U * I;
+	return P;
+}
