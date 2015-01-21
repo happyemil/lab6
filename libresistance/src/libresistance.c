c28e1a8495cbfd16212e1e09c87f68ac2dd5a385
Fredrik Johansson <fredrik.johansson.c@gmail.com>
2015-01-21 15:02:43 +0100

    libresistance ready, but not tested.

--- libresistance/src/libresistance.c
+++ libresistance/src/libresistance.c
@@ -1,1 +1,31 @@
-// test
+
+
+
+float calc_resistance(int count, char conn, float *array)
+{
+  float retVal = 0;
+
+  if (count == 0)
+    return 0; // Avoid divide by zero in parallel calculation
+
+  int i = 0;
+  if (conn == 'p')
+  {
+    float tmpSum = 0;
+    for (i = 0; i < count; i++)
+    {
+      tmpSum += 1 / array[i];
+    }
+
+    retVal = 1 / tmpSum;
+  }
+  else
+  {
+    for (i = 0; i < count; i++)
+    {
+      retVal += array[i];      
+    }
+  }
+
+  return retVal;
+}
